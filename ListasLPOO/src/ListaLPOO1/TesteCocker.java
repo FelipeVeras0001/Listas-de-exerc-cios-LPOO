169d6815642476d9b6c290d4fd0e8dc30ee2173a
FelipeVeras0001 <33816819+FelipeVeras0001@users.noreply.github.com>
2017-11-25 23:07:16 -0200

    Add files via upload

---
+++ ListasLPOO/src/ListaLPOO1/TesteCocker.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ListaLPOO1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author EDUARDO
+ */
+public class TesteCocker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Cocker c=new Cocker(true, null, null, null, null);
+        c.setNome(JOptionPane.showInputDialog(null,"Digite o nome"));
+        c.setCor(JOptionPane.showInputDialog(null,"Digite a cor"));
+        c.setRaca(JOptionPane.showInputDialog(null,"Digite a raça"));
+        c.setTipo(JOptionPane.showInputDialog(null,"Digite o tipo"));
+        c.setTosa(true);
+        JOptionPane.showMessageDialog(null, c);
+
+        
+                
+    }
+    
+}
---
+++ ListasLPOO/src/listaLPOO3/CartaoWeb.java
@@ -0,0 +1,17 @@
+package listaLPOO3;
+
+
+
+public abstract class CartaoWeb {
+	protected String destinatario;
+	
+	
+        public String retornarMensagem(String remetente) {
+		return null;
+		
+	}
+
+	public CartaoWeb(String destinatario) {
+		this.destinatario = destinatario;
+	}
+}
