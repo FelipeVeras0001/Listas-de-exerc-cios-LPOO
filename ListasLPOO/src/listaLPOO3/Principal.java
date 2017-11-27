9638e74e95b55643097d6a58969e6fa497b02415
FelipeVeras0001 <33816819+FelipeVeras0001@users.noreply.github.com>
2017-11-27 17:31:53 -0200

    Update Principal.java

--- ListasLPOO/src/listaLPOO3/Principal.java
+++ ListasLPOO/src/listaLPOO3/Principal.java
@@ -7,14 +7,14 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		cartoes = new CartaoWeb[3];
-		DiaDosNamorados cartao1 = new DiaDosNamorados("Fulana");
+		DiaDosNamorados cartao1 = new DiaDosNamorados("Mariana");
 		cartoes [0] = cartao1;
-		Natal cartao2 = new Natal("Cicrano");
+		Natal cartao2 = new Natal("Carlos Augusto");
 		cartoes[1] = cartao2;
-		Aniversario cartao3 = new Aniversario("Deltrano");
+		Aniversario cartao3 = new Aniversario("José");
 		cartoes[2] = cartao3;
 		for(int i = 0; i < cartoes.length; i++) {
-			JOptionPane.showMessageDialog(null, cartoes[i].retornarMensagem("Marcelo"));
+			JOptionPane.showMessageDialog(null, cartoes[i].retornarMensagem("Felipe"));
 		}
 	}
 
